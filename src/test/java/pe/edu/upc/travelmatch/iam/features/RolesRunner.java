c04cc8bb9abdce7904711c495421164df1847329
David Joel Morales Venegas <davidmvjoel@outlook.es>
2026-05-10 17:27:33 -0500

    test: fix integration tests structure

---
+++ src/test/java/pe/edu/upc/travelmatch/iam/features/RolesRunner.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.travelmatch.iam.features;
+
+import com.intuit.karate.junit5.Karate;
+
+public class RolesRunner {
+    @Karate.Test
+    Karate testRoles() {
+        return Karate.run("roles").relativeTo(getClass());
+    }
+}
